f31977f63aed8c896c6c19e2e66b2cf84c84e5d4
desmarchris <c.desmarais@rasa.com>
2020-12-21 17:58:04 -0500

    survey only fires once now

--- actions/actions.py
+++ actions/actions.py
@@ -12,6 +12,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
+from rasa_sdk.events import FollowupAction
 
 
 class ActionProductSearch(Action):
@@ -27,4 +28,32 @@ class ActionProductSearch(Action):
         reset_slots = ["number", "color"]
         return [SlotSet(slot, None) for slot in reset_slots]
         # return []
+
+class SurveyStart(Action):
+
+    def name(self) -> Text:
+        return "action_survey_start"
+
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        if tracker.get_slot("survey_complete")==True:
+            return[]
+        else: 
+            return [FollowupAction("survey_form")]
+
+class SurveySubmit(Action):
+
+    def name(self) -> Text:
+        return "action_survey_submit"
+
+    async def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        dispatcher.utter_message(template = "utter_open_feedback")
+        dispatcher.utter_message(template = "utter_survey_end")
+        return [SlotSet("survey_complete", True)]
+
 
