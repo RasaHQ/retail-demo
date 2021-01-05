1a7023b336642d801e69ab8ee1e9be5565ef8dd4
desmarchris <c.desmarais@rasa.com>
2021-01-05 14:00:22 -0500

    sqlite integration added

--- actions/actions.py
+++ actions/actions.py
@@ -13,7 +13,7 @@ from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from rasa_sdk.events import FollowupAction
-
+import sqlite3
 
 class ActionProductSearch(Action):
 
@@ -24,10 +24,32 @@ class ActionProductSearch(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        dispatcher.utter_message(template = "utter_product_stock_finish")
-        reset_slots = ["number", "color"]
-        return [SlotSet(slot, None) for slot in reset_slots]
-        # return []
+        # connect to DB
+        conn = sqlite3.connect('example.db')
+        c = conn.cursor()        
+
+        # get slots
+        shoe = [
+            (tracker.get_slot('color')),
+            (tracker.get_slot('number'))
+        ]        
+
+        # retrieve row based on search criteria
+        c.execute('SELECT * FROM inventory WHERE color=? AND size=?', shoe)
+        rw = c.fetchone()
+
+        if rw:
+            # provide in stock message
+            dispatcher.utter_message(template = "utter_in_stock")
+            conn.close()
+            reset_slots = ["number", "color"]
+            return [SlotSet(slot, None) for slot in reset_slots]
+        else: 
+            # provide out of stock
+            dispatcher.utter_message(template = "utter_no_stock")
+            conn.close()
+            reset_slots = ["number", "color"]
+            return [SlotSet(slot, None) for slot in reset_slots]
 
 class SurveyStart(Action):
 
@@ -41,7 +63,7 @@ class SurveyStart(Action):
         if tracker.get_slot("survey_complete")==True:
             return[]
         else: 
-            return [FollowupAction("survey_form")]
+            return [FollowupAction("survey_form"), SlotSet("number", None)]
 
 class SurveySubmit(Action):
 
@@ -54,6 +76,116 @@ class SurveySubmit(Action):
 
         dispatcher.utter_message(template = "utter_open_feedback")
         dispatcher.utter_message(template = "utter_survey_end")
-        return [SlotSet("survey_complete", True)]
+        return [SlotSet("survey_complete", True), SlotSet("number", None)]
+
+class OrderStatus(Action):
+    
+    def name(self) -> Text:
+        return "action_order_status"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # connect to DB
+        conn = sqlite3.connect('example.db')
+        c = conn.cursor()        
+
+        # get email slot
+        order_email = (tracker.get_slot('email'),)
+
+        # retrieve row based on email
+        c.execute('SELECT * FROM orders WHERE order_email=?', order_email)
+        rw = c.fetchone()
+
+        if rw:
+            rw_lst = list(rw)
+
+            # respond with order status
+            dispatcher.utter_message(template = "utter_order_status", status = rw_lst[5])
+            conn.close()
+            return []
+        else: 
+            # db didn't have an entry with this email
+            dispatcher.utter_message(template = "utter_no_order")
+            conn.close()
+            return []  
 
 
+class CancelOrder(Action):
+    
+    def name(self) -> Text:
+        return "action_cancel_order"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # connect to DB
+        conn = sqlite3.connect('example.db')
+        c = conn.cursor()        
+
+        # get email slot
+        order_email = (tracker.get_slot('email'),)
+
+        # retrieve row based on email
+        c.execute('SELECT * FROM orders WHERE order_email=?', order_email)
+        rw = c.fetchone()
+
+        if rw:
+            # change status of entry
+            status = [
+                ('cancelled'),
+                (tracker.get_slot('email'))
+            ]
+            c.execute('UPDATE orders SET status=? WHERE order_email=?', status)
+            conn.commit()
+            conn.close()
+
+            # confirm cancellation
+            dispatcher.utter_message(template = "utter_order_cancel_finish")
+            return []
+        else: 
+            # db didn't have an entry with this email
+            dispatcher.utter_message(template = "utter_no_order")
+            conn.close()
+            return []  
+
+class ReturnOrder(Action):
+    
+    def name(self) -> Text:
+        return "action_return"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        # connect to DB
+        conn = sqlite3.connect('example.db')
+        c = conn.cursor()        
+
+        # get email slot
+        order_email = (tracker.get_slot('email'),)
+
+        # retrieve row based on email
+        c.execute('SELECT * FROM orders WHERE order_email=?', order_email)
+        rw = c.fetchone()
+
+        if rw:
+            # change status of entry
+            status = [
+                ('returning'),
+                (tracker.get_slot('email'))
+            ]
+            c.execute('UPDATE orders SET status=? WHERE order_email=?', status)
+            conn.commit()
+            conn.close()
+
+            # confirm return
+            dispatcher.utter_message(template = "utter_return_finish")
+            return []
+        else: 
+            # db didn't have an entry with this email
+            dispatcher.utter_message(template = "utter_no_order")
+            conn.close()
+            return []  
