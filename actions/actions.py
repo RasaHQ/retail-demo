5018ced9aea7d1a858f97ce61a85584134f415d5
Mikhail <isachenko.mikhail.k@gmail.com>
2022-11-14 18:27:07 +0300

    Add create order action

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,7 @@ from rasa_sdk.executor import CollectingDispatcher
 from rasa_sdk.events import SlotSet
 from rasa_sdk.events import FollowupAction
 from rasa_sdk.events import BotUttered
+from datetime import datetime
 import sqlite3
 
 # change this to the location of your SQLite file
@@ -28,7 +29,7 @@ class ActionProductSearch(Action):
         shoe = [(tracker.get_slot("color")), (tracker.get_slot("size"))]
 
         # place cursor on correct row based on search criteria
-        cursor.execute("SELECT * FROM inventory WHERE color=? AND size=?", shoe)
+        cursor.execute("SELECT * FROM inventory WHERE color=? AND size=? AND Count > 0", shoe)
         
         # retrieve sqlite row
         data_row = cursor.fetchone()
@@ -81,7 +82,7 @@ class OrderStatus(Action):
         order_email = (tracker.get_slot("email"),)
 
         # retrieve row based on email
-        cursor.execute("SELECT * FROM orders WHERE order_email=?", order_email)
+        cursor.execute("SELECT * FROM orders join user on user.id = user WHERE email=?", order_email)
         data_row = cursor.fetchone()
 
         if data_row:
@@ -118,13 +119,13 @@ class CancelOrder(Action):
         order_email = (tracker.get_slot("email"),)
 
         # retrieve row based on email
-        cursor.execute("SELECT * FROM orders WHERE order_email=?", order_email)
+        cursor.execute("SELECT * FROM orders join user on user.id = user WHERE email=?", order_email)
         data_row = cursor.fetchone()
 
         if data_row:
             # change status of entry
             status = [("cancelled"), (tracker.get_slot("email"))]
-            cursor.execute("UPDATE orders SET status=? WHERE order_email=?", status)
+            cursor.execute("UPDATE orders SET status=? WHERE user=(SELECT id FROM user WHERE email=?)", status)
             connection.commit()
             connection.close()
 
@@ -157,13 +158,13 @@ class ReturnOrder(Action):
         order_email = (tracker.get_slot("email"),)
 
         # retrieve row based on email
-        cursor.execute("SELECT * FROM orders WHERE order_email=?", order_email)
+        cursor.execute("SELECT * FROM orders join user on user.id = user WHERE email=?", order_email)
         data_row = cursor.fetchone()
 
         if data_row:
             # change status of entry
             status = [("returning"), (tracker.get_slot("email"))]
-            cursor.execute("UPDATE orders SET status=? WHERE order_email=?", status)
+            cursor.execute("UPDATE orders SET status=? WHERE user=(SELECT id FROM user WHERE email=?)", status)
             connection.commit()
             connection.close()
 
@@ -195,3 +196,40 @@ class GiveName(Action):
         )
 
         return [evt]
+
+class CreateOrder(Action):
+        def name(self) ->  Text:
+            return "action_create_order"
+
+        def run(
+            self,
+            dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any],
+        ) -> List[Dict[Text, Any]]:
+
+            try:
+                connection = sqlite3.connect(path_to_db)
+                cursor = connection.cursor()
+                user_email = tracker.get_slot("email")
+                color = tracker.get_slot("color")
+                size = tracker.get_slot("size")
+                
+                cursor.execute("SELECT * FROM user where email=?", (user_email, ))
+                data_row = cursor.fetchone()
+
+                if not data_row:
+                    cursor.execute("INSERT INTO user (email) VALUES (?)", (user_email, ))
+                    connection.commit()
+                    cursor.execute("SELECT * FROM user where email=?", (user_email, ))
+                    data_row = cursor.fetchone()
+
+                user_id =  list(data_row)[0]
+                cursor.execute("INSERT into orders (order_date, user, color, size, status) values (?,?,?,?,?)", (str(datetime.now().date()), user_id, color, size, 2))
+                cursor.execute("UPDATE inventory SET count=count-1 WHERE color=? AND size=?", (color, size))
+                connection.commit()
+                connection.close()
+                dispatcher.utter_message(template="utter_order_create_finish")
+
+            except Exception as e:
+                dispatcher.utter_message(template="utter_order_create_error")
--- initialize_db.py
+++ initialize_db.py
@@ -13,38 +13,65 @@ c = conn.cursor()
 
 # EXISTING ORDERS
 # Create table
+
+c.execute('''CREATE TABLE status 
+            (id INTEGER PRIMARY KEY AUTOINCREMENT, name)''')
+
+statuses = [
+    ('shipped',),
+    ('order pending',),
+    ('delivered',)
+]
+
+c.executemany('INSERT INTO status (name) VALUES(?)', statuses)
+
+
+c.execute('''CREATE TABLE user
+        (id INTEGER PRIMARY KEY AUTOINCREMENT, email)''')
+
+users = [
+    ('example@rasa.com',),
+    ('me@rasa.com',),
+    ('me@gmail.com',)
+]
+
+c.executemany('INSERT INTO user (email) VALUES (?)', users)
+
+
 c.execute('''CREATE TABLE orders
-             (order_date, order_number, order_email, color, size, status)''')
+             (order_number INTEGER PRIMARY KEY AUTOINCREMENT, order_date, user, color, size, status,
+             FOREIGN KEY(user) REFERENCES user(id),
+             FOREIGN KEY(status) REFERENCES status(id))''')
 
 # data to be added
-purchases = [('2006-01-05',123456,'example@rasa.com','blue', 9, 'shipped'),
-             ('2021-01-05',123457,'me@rasa.com','black', 10, 'order pending'),
-             ('2021-01-05',123458,'me@gmail.com','gray', 11, 'delivered'),
+purchases = [('2006-01-05',1,'blue', 9, 1),
+             ('2021-01-05',2,'black', 10, 2),
+             ('2021-01-05',3,'gray', 11, 3),
             ]
 
 # add data
-c.executemany('INSERT INTO orders VALUES (?,?,?,?,?,?)', purchases)
+c.executemany('INSERT INTO orders (order_date, user, color, size, status) VALUES (?,?,?,?,?)', purchases)
 
 # AVAILABLE INVENTORY
 # Create table
 c.execute('''CREATE TABLE inventory
-             (size, color)''')
+             (size, color, count)''')
 
 # data to be added
-inventory = [(7, 'blue'),
-             (8, 'blue'),
-             (9, 'blue'),
-             (10, 'blue'),
-             (11, 'blue'),
-             (12, 'blue'),
-             (7, 'black'),
-             (8, 'black'),
-             (9, 'black'),
-             (10, 'black')
+inventory = [(7, 'blue', 1),
+             (8, 'blue', 3),
+             (9, 'blue', 0),
+             (10, 'blue', 1),
+             (11, 'blue', 0),
+             (12, 'blue', 3),
+             (7, 'black', 3333),
+             (8, 'black', 1),
+             (9, 'black', 0),
+             (10, 'black', 1)
             ]
 
 # add data
-c.executemany('INSERT INTO inventory VALUES (?,?)', inventory)
+c.executemany('INSERT INTO inventory VALUES (?,?,?)', inventory)
 
 
 # Save (commit) the changes
